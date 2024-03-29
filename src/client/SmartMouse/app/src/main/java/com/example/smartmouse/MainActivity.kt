d02183c40c793281fba088293c2e8569de530d63
minusno0708 <naoyao1014@gmail.com>
2024-03-29 15:51:27 +0900

    feat:マウス画面にマウス操作機能を移行

--- src/client/SmartMouse/app/src/main/java/com/example/smartmouse/MainActivity.kt
+++ src/client/SmartMouse/app/src/main/java/com/example/smartmouse/MainActivity.kt
@@ -10,16 +10,7 @@ import com.example.smartmouse.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var sensorManager: SensorManager
-
-    private lateinit var gyroscopeManager: GyroscopeManager
-    private lateinit var gyroscopeListener: GyroscopeListener
-
-    private lateinit var accelerometerManager: AccelerometerManager
-    private lateinit var accelerometerListener: AccelerometerListener
-
     private val serverManager = ServerManager()
-    private val controller = OutputController(serverManager)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,59 +21,12 @@ class MainActivity : AppCompatActivity() {
             toMouse()
         }
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-        gyroscopeListener = GyroscopeListener()
-        gyroscopeManager = GyroscopeManager(sensorManager, gyroscopeListener)
-
-        accelerometerListener = AccelerometerListener()
-        accelerometerManager = AccelerometerManager(sensorManager, accelerometerListener)
-
         serverManager.connect("192.168.11.64", 11000)
-        controller.mouseEnable()
     }
 
     private fun toMouse() {
         val intent = Intent(this, MouseActivity::class.java)
         startActivity(intent)
-    }
-
-    inner class GyroscopeListener: GyroscopeManager.GyroscopeListener {
-        override fun onValueChanged(x: Float, y: Float, z: Float) {
-            updateGyroValues(x, y, z)
-        }
-    }
-
-    private fun updateGyroValues(x: Float, y: Float, z: Float) {
-        controller.setMoveCoo(
-            (-z*100).toInt(),
-            (-x*100).toInt()
-        )
-    }
-
-    inner class AccelerometerListener: AccelerometerManager.AccelerometerListener {
-        override fun onValueChanged(x: Float, y: Float, z: Float) {
-            updateAccValues(x, y, z)
-        }
-    }
-
-    private fun updateAccValues(x: Float, y: Float, z: Float) {
-    }
-
-    private fun valueRound(value: Float): Float {
-        return Math.round(value * 100f) / 100f
-    }
-
-    override fun onResume() {
-        super.onResume()
-        gyroscopeManager.registerListener()
-        accelerometerManager.registerListener()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        gyroscopeManager.unregisterListener()
-        accelerometerManager.unregisterListener()
     }
 
     private fun updateLog(message: String) {
--- src/client/SmartMouse/app/src/main/java/com/example/smartmouse/MouseActivity.kt
+++ src/client/SmartMouse/app/src/main/java/com/example/smartmouse/MouseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartmouse
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,17 @@ import com.example.smartmouse.databinding.ActivityMouseBinding
 class MouseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMouseBinding
 
+    private lateinit var sensorManager: SensorManager
+
+    private lateinit var gyroscopeManager: GyroscopeManager
+    private lateinit var gyroscopeListener: MouseActivity.GyroscopeListener
+
+    private lateinit var accelerometerManager: AccelerometerManager
+    private lateinit var accelerometerListener: MouseActivity.AccelerometerListener
+
+    private val serverManager = ServerManager()
+    private val controller = OutputController(serverManager)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMouseBinding.inflate(layoutInflater)
@@ -19,10 +32,59 @@ class MouseActivity : AppCompatActivity() {
         binding.toHome.setOnClickListener {
             toHome()
         }
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        gyroscopeListener = GyroscopeListener()
+        gyroscopeManager = GyroscopeManager(sensorManager, gyroscopeListener)
+
+        accelerometerListener = AccelerometerListener()
+        accelerometerManager = AccelerometerManager(sensorManager, accelerometerListener)
+
+        serverManager.connect("192.168.11.64", 11000)
+        controller.mouseEnable()
     }
 
     private fun toHome() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    inner class GyroscopeListener: GyroscopeManager.GyroscopeListener {
+        override fun onValueChanged(x: Float, y: Float, z: Float) {
+            updateGyroValues(x, y, z)
+        }
+    }
+
+    private fun updateGyroValues(x: Float, y: Float, z: Float) {
+        controller.setMoveCoo(
+            (-z*100).toInt(),
+            (-x*100).toInt()
+        )
+    }
+
+    inner class AccelerometerListener: AccelerometerManager.AccelerometerListener {
+        override fun onValueChanged(x: Float, y: Float, z: Float) {
+            updateAccValues(x, y, z)
+        }
+    }
+
+    private fun updateAccValues(x: Float, y: Float, z: Float) {
+    }
+
+    private fun valueRound(value: Float): Float {
+        return Math.round(value * 100f) / 100f
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gyroscopeManager.registerListener()
+        accelerometerManager.registerListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gyroscopeManager.unregisterListener()
+        accelerometerManager.unregisterListener()
     }
 }
